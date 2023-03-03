d19167d1bda14f249680064ead11375bb5a694f5
purupuruputtyan <ec2-user@ip-172-31-7-79.ap-northeast-1.compute.internal>
2023-03-03 14:03:32 +0000

    [Finish]2-6

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,37 @@
 class ListsController < ApplicationController
+
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
   end
 
   def index
+    @lists = List.all
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to list_path(list.id)
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
   end
 end
